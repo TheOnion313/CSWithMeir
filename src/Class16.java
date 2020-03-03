5f2e3b3689fbe032642dc101cb564efe4cf56a2e
TheOnion313 <royn77777@gmail.com>
2020-03-03 13:31:48 +0200

    Class16 - linesIntersect not working

--- src/Class16.java
+++ src/Class16.java
@@ -1,4 +1,5 @@
 import basics.Student;
+import canvasML.Line;
 
 public class Class16 {
 
@@ -56,9 +57,39 @@ public class Class16 {
         return copy;
     }
 
+    public static boolean linesIntersect(int[][] ln1, int[][] ln2) {
+        int[] p1 = ln1[0];
+        double m1 = (double)(p1[1] - ln2[0][1]) / (double)(p1[0] - ln2[0][0] + Math.pow(10, -10));
+        double m2 = (double)(p1[1] - ln2[1][1]) / (double)(p1[0] - ln2[1][0] + Math.pow(10, -10));
+        double m = (double)(p1[1] - ln1[1][1]) / (double)(p1[0] - ln1[1][0] + Math.pow(10, -10));
+
+        return m > Math.max(m1, m2) || m < Math.min(m1, m2);
+    }
+
+    public static boolean isPoly(int[][] poly) {
+        boolean is = true;
+        for (int i = 0; i < poly.length - 1; i++) {
+            for (int j = 0; j < poly.length - 1; j++) {
+                if (i != j && linesIntersect(new int[][]{poly[i], poly[i + 1]}, new int[][]{poly[j], poly[j + 1]})) {
+                    is = false;
+                }
+            }
+        }
+        return is;
+    }
+
+    public static void drawPoly(int[][] poly) {
+        for (int i = 0; i < poly.length - 1; i++) {
+            Line l = new Line(poly[i][0], poly[i][1], poly[i + 1][0], poly[i + 1][1], "black");
+        }
+        Line l = new Line(poly[0][0], poly[0][1], poly[poly.length - 1][0], poly[poly.length - 1][1], "black");
+    }
+
 
     public static void main(String[] args) {
-
+        int[][] poly = new int[][]{{50, 50}, {100, 100}, {150, 50}, {85, 75}};
+        System.out.println(isPoly(poly));
+        drawPoly(poly);
     }
 
 }
